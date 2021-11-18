0afa76b792f5288cc0df663d4c7653e58025ddf0
Arthur Leandro <arthurdocsgo@gmail.com>
2021-11-18 16:47:28 -0300

    Project transcript to deploy

---
+++ src/test/java/com/BlogPessoal/App/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogPessoal.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
